b42a6d21d74ab850c983feba1a8c0a21695bdb86
shahjahan <shahjahanattari26@gmail.com>
2021-07-09 16:57:47 +0600

    db change

--- db/connect.php
+++ db/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$db = "eventmgmt";
+$username = "eventsite";
+$password = "eventsite";
+$db = "eventsite";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
